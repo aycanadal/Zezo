e8b5bcfd73389bc3a579d5941d104b6fe0d5b9c7
Aycan <Aycan@Corsair>
2014-10-12 19:31:14 +0300

    Part 2

--- ZezoMusicPlayer/src/com/zezo/zezomusicplayer/MainActivity.java
+++ ZezoMusicPlayer/src/com/zezo/zezomusicplayer/MainActivity.java
@@ -1,16 +1,82 @@
 package com.zezo.zezomusicplayer;
 
+
+import android.os.IBinder;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.zezo.zezomusicplayer.MusicService.MusicBinder;
+
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private MusicService musicSrv;
+	private Intent playIntent;
+	private boolean musicBound=false;
+	
+	private ArrayList<Song> songList;
+	private ListView songView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		songView = (ListView)findViewById(R.id.song_list);
+		songList = new ArrayList<Song>();
+		getSongList();
+		Collections.sort(songList, new Comparator<Song>(){
+			  public int compare(Song a, Song b){
+			    return a.getTitle().compareTo(b.getTitle());
+			  }
+			});
+		
+		SongAdapter songAdt = new SongAdapter(this, songList);
+		songView.setAdapter(songAdt);
+	}
+	
+	//connect to the service
+	private ServiceConnection musicConnection = new ServiceConnection(){
+	 
+	  @Override
+	  public void onServiceConnected(ComponentName name, IBinder service) {
+	    MusicBinder binder = (MusicBinder)service;
+	    //get service
+	    musicSrv = binder.getService();
+	    //pass list
+	    musicSrv.setList(songList);
+	    musicBound = true;
+	  }
+	 
+	  @Override
+	  public void onServiceDisconnected(ComponentName name) {
+	    musicBound = false;
+	  }
+	};
+	
+	@Override
+	protected void onStart() {
+	  super.onStart();
+	  if(playIntent==null){
+	    playIntent = new Intent(this, MusicService.class);
+	    boolean bound = bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+	    startService(playIntent);	    
+	  }
 	}
 
 	@Override
@@ -22,13 +88,54 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+		case R.id.action_shuffle:
+		  //shuffle
+		  break;
+		case R.id.action_end:
+		  stopService(playIntent);
+		  musicSrv=null;
+		  System.exit(0);
+		  break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void getSongList() {
+		  //retrieve song info
+		ContentResolver musicResolver = getContentResolver();
+		Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+		Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+		if(musicCursor!=null && musicCursor.moveToFirst()){
+			  //get columns
+			  int titleColumn = musicCursor.getColumnIndex
+			    (android.provider.MediaStore.Audio.Media.TITLE);
+			  int idColumn = musicCursor.getColumnIndex
+			    (android.provider.MediaStore.Audio.Media._ID);
+			  int artistColumn = musicCursor.getColumnIndex
+			    (android.provider.MediaStore.Audio.Media.ARTIST);
+			  //add songs to list
+			  do {
+			    long thisId = musicCursor.getLong(idColumn);
+			    String thisTitle = musicCursor.getString(titleColumn);
+			    String thisArtist = musicCursor.getString(artistColumn);
+			    songList.add(new Song(thisId, thisTitle, thisArtist));
+			  }
+			  while (musicCursor.moveToNext());
+			}
+		}	
+	
+	public void songPicked(View view) {
+		musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+		musicSrv.playSong();
+		}
+	
+	@Override
+	protected void onDestroy() {
+	  stopService(playIntent);
+	  musicSrv=null;
+	  super.onDestroy();
+	}
+	
+	
 }
