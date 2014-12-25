27a6da3985934908f257518d6e06206554e7181c
Aycan <Aycan@Corsair>
2014-12-25 20:44:13 +0200

    target 14 proguard

--- ZezoMusicPlayer/src/com/zezo/zezomusicplayer/MainActivity.java
+++ ZezoMusicPlayer/src/com/zezo/zezomusicplayer/MainActivity.java
@@ -480,7 +480,7 @@ public class MainActivity extends ActionBarActivity implements SearchListener,
 
 		super.onCreate(savedInstanceState);
 
-		SpannableString s = new SpannableString("Zezo v0.4.16");
+		SpannableString s = new SpannableString("Zezo a0.1");
 		s.setSpan(new TypefaceSpan(this, "Action_Man.ttf"), 0, s.length(),
 				Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
